12cbc0ab0e406253a773d141102f984261524117
youssefoukani <youssefoukani@gmail.com>
2024-06-26 18:46:17 +0200

    modifica estetica pag login

--- progetto_serio/vite-project/src/components/Login.jsx
+++ progetto_serio/vite-project/src/components/Login.jsx
@@ -4,6 +4,10 @@ import { useNavigate } from 'react-router-dom';
 import "./Register.css"
 import React, { useState } from "react";
 
+import password_icon from '../assets/password.png'
+import email_icon from '../assets/person.png'
+
+
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -46,18 +50,26 @@ export default function Login() {
   return (
     <div className="login">
       <div className="login-container">
-        <h2>Accedi come {status}</h2>
+        <h2>Accedi</h2>
         <div className="form-group">
           <form onSubmit={handleSubmit} action="input" method="post">
-            <input
+            <div className="inputs">
+
+            <div className="input">
+              <img src={email_icon} alt=""/>
+              <input
               type="text"
               id="email"
               name="email"
               onChange={(e) => setEmail(e.target.value)}
               required
               placeholder="E-mail"
-            />
-            <input
+              />
+            </div>
+
+            <div className="input">
+              <img src={password_icon} alt=""/>
+              <input
               type="password"
               id="password"
               name="password"
@@ -65,22 +77,30 @@ export default function Login() {
               placeholder="Password"
               required
             />
-
+            </div>
+            
+            
+            </div>
+            
+            <div class="custom-select-container">
             <select
               id="status"
               name="status"
+              class="custom-select"
               onChange={(e) => setStatus(e.target.value)}
               required
             >
               <option value="privato">Privato</option>
               <option value="azienda">Azienda</option>
             </select>
+            </div>
+            
 
             <button type="submit">Accedi</button>
           </form>
         </div>
-        <div className="registrato">
-          <a href="/register">Non hai un account? Registrati</a>
+        <div className="domanda">Non hai un account?
+          <a href="/register"> Registrati</a>
         </div>
       </div>
     </div>
