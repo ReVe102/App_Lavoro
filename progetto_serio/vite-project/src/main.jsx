296f043f6cd1375c836494c0ee0bc27d2fee7d8b
youssefoukani <youssefoukani@gmail.com>
2024-07-04 12:20:29 +0200

    commenti orale

--- progetto_serio/vite-project/src/main.jsx
+++ progetto_serio/vite-project/src/main.jsx
@@ -7,3 +7,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App/>
   
 )
+
+
+//•	document.getElementById('root'): Ottiene l’elemento DOM con l’ID ‘root’, che è un div nel file index.HTML principale dell’app.
+//ReactDOM.createRoot(...).render(...): Crea una radice React nel nodo DOM specificato e renderizza il componente App al suo interno.
+
+// Quindi, in sintesi, questo codice imposta la struttura di base per un’applicazione React. Importa le librerie necessarie, 
+//importa il componente principale App, e quindi lo rende all’interno dell’elemento DOM con l’ID root.
--- progetto_serio/vite-project/vite.config.js
+++ progetto_serio/vite-project/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'  //definire configurazione progetto
+import react from '@vitejs/plugin-react'  //importare il plugin react per vite
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,3 +9,7 @@ export default defineConfig({
     open: true, 
   },
 });
+
+
+//In sintesi, questo file configura un progetto Vite per utilizzare React, 
+//eseguendo il server di sviluppo sulla porta 5173 e aprendo automaticamente il browser quando il server viene avviato.
